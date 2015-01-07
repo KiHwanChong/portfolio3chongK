bca030ed60d102664692032fd0e8ef62f561394b
Ki Hwan Chong <joelkh784@gmail.com>
2015-01-07 11:25:31 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -5,3 +5,27 @@
  */
 
 
+$("document").ready(function(){
+    $(".haha").css("background-color", "yellow");
+    $("p").css("background-color", "gray");
+    $('#oneButton').bind('click', alertButtonClick);
+    $('#textBox1').bind('blur', onBlurEvent)
+            .bind('onmousedown', onMDownEvent)
+            .bind('onmouseup', onMUpEvent);
+});
+
+function alertButtonClick(){
+       alert("There was a button clicked");   
+}
+
+function onBlurEvent(){
+    $("#second").html("You left the box");
+}
+
+function onMDownEvent(){
+    $("#second").html("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+}
+
+function onMUpEvent(){
+    $("#second").html("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+}
